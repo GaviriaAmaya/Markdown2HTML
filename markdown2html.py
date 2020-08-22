5c6d40de83529fd178f6fb042fc7392d54f34084
GaviriaAmaya <729@holbertonschool.com>
2020-08-22 11:05:20 -0500

    [Add] Markdown Initial Files

---
+++ markdown2html.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+" Starter script for the Markdown2Html "
+from sys import argv
+
+if __name__ == "__main__":
+    if argv != 2:
+        raise SyntaxError
+    if argv[1] is not file:
+        raise FileNotFoundError
